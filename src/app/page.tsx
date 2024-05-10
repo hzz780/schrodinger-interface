76de3d0190a154265e988957fac92b8cfc05da8b
hzz780 <hzz780@qq.com>
2024-05-10 18:08:50 +0800

    test: 💍 next lint demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+
+const hello = 'hi next lint';
+console.log('hello, maybe a lint warning:', hello);
+
 export default dynamic(() => import('pageComponents/tokensPage'), { ssr: false });
